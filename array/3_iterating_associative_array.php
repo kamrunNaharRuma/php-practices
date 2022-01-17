cd8fabe38bd311a3fd0412c1e0da02dd28677d3e
knahar12bd@gmail.com <knahar12bd@gmail.com>
2022-01-17 13:14:46 +0600

    array functions

---
+++ array/3_iterating_associative_array.php
@@ -0,0 +1,11 @@
+<?php
+
+$myself = array(
+    "occupation" => "software engineer",
+    "birth day " => "12 th May"
+);
+
+foreach ($myself as $key => $value) {
+    echo $key . " : " . $value;
+    echo "  .  ";
+}
